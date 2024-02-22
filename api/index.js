66d14009e5042299b4be305ae7f7a7de9bc10c44
BHUVANESH G <bhuvaneshcse03@gmail.com>
2024-02-22 19:19:07 +0530

    fourth commit

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://library-management-1-e7u3.onrender.com/",
   })
 );
 
